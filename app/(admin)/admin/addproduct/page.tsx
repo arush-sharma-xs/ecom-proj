62e0445270b1ccc3b6c0c2eb72e2c67da0302bf9
Arush Sharma <arush.sharma@xenonstack.com>
2024-07-18 18:52:38 +0530

    improve form design

--- app/(admin)/admin/addproduct/page.tsx
+++ app/(admin)/admin/addproduct/page.tsx
@@ -1,35 +1,41 @@
 "use client"
 
-import React from 'react'
+import React, {useRef} from 'react'
 
 export default function page() {
   // title, desc, category, price, stock
-
+  const productImageRef = useRef();
   
   const handleSubmit = () => {
     console.log("willhandle submit")
 
   }
+
+  const handleProductImageUpload = () => {
+    if(productImageRef && productImageRef.current) {
+      productImageRef.current.click()
+    }  
+  }
   
   return (
-    <div className="flex p-2 flex-col">
-      <h2>Add Product Form</h2>
-        <form action="#" onSubmit={handleSubmit} className="w-[500px]">
+    <div className="flex p-2 flex-col items-center gap-2">
+      <h2 className='text-xl font-bold'>Add Product Form</h2>
+        <form action="#" onSubmit={handleSubmit} className="w-[500px] flex flex-col gap-2">
             <div className="flex flex-col gap-2">
               <label htmlFor="title">Product Title </label>
-              <input type="text" name="productTitle" placeholder="Enter Product Title"/>
+              <input type="text" name="productTitle" placeholder="Enter Product Title" className='bg-gray-100 p-2 rounded-md'/>
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="title">Product Description </label>
-              <input type="text" name="productDesc" placeholder="Enter Product Description"/>
+              <input type="text" name="productDesc" placeholder="Enter Product Description" className='bg-gray-100 p-2 rounded-md'/>
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="title">Product price </label>
-              <input type="text" name="productDesc" placeholder="Enter Product Description"/>
+              <input type="text" name="productDesc" placeholder="Enter Product Description" className='bg-gray-100 p-2 rounded-md'/>
             </div>
             <div className="flex flex-col gap-2">
               <label htmlFor="title">Product Category </label>
-              <select name="productCategory">
+              <select name="productCategory" className='bg-gray-100 p-2 rounded-md'>
                   <option value="electronic">Electronics</option>
                   <option value="jewelery">Jewelery</option>
                   <option value="men's clothing">Men's clothing</option>
@@ -39,12 +45,14 @@ export default function page() {
 
             <div className="flex flex-col gap-2">
               <label htmlFor="title">Product Image </label>
-              <input type="file" name="productImages" accept="image/*"  />
+              <div onClick={handleProductImageUpload} className="bg-gray-100 w-[500px] h-[50px] flex items-center justify-center text-black/70 rounded-md cursor-pointer">
+                + Click here to Add Product Images
+                <input ref={productImageRef} type="file" className='hidden' name="productImages" accept="image/*"  />
+              </div>
             </div>
             
-          <button className="p-2 m-2 bg-green-300 rounded-lg font-bold" type="submit">Add Product </button>
+          <button type="submit" className="p-2 m-2 bg-green-300 rounded-lg font-bold" type="submit">Add Product </button>
         </form>
-
     </div>
   )
 }
--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <h2>Welcome to Admin Panel</h2>
+    <div className="flex flex-col w-full items-center gap-2 mt-8">
+      <h2 className="text-xl font-bold">Welcome to Admin Panel</h2>
       <ul className="flex gap-2">
         <li>
           <Link href="/admin/addproduct" className="p-2 bg-blue-300 text-black rounded-md font-bold">Add Product</Link>
@@ -13,7 +13,6 @@ export default function layout({ children }: { children: React.ReactNode }) {
           <Link href="/admin/listproduct" className="p-2 bg-blue-300 text-black rounded-md font-bold">List Product</Link>
         </li>
       </ul>
-
       <div className="mt-4">{children}</div>
     </div>
   );
